00c891bab226416ffbae1332caf2a4dc8a6b85c1
MouadSadik <sadikmoad90@gmail.com>
2025-09-12 16:56:26 +0100

    ujikjn

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -9,85 +9,85 @@ export function Projects() {
   return (
     <div>
       {/*Header */}
-<motion.div
-  initial={{ opacity: 0, y: 20 }}
-  whileInView={{ opacity: 1, y: 0 }}
-  viewport={{ once: true }}
-  transition={{ duration: 0.5 }}
-  className="mb-12 flex flex-col items-center justify-center space-y-4 text-center"
->
-  <Badge
-    className="rounded-full px-4 py-1.5 text-sm font-medium shadow-sm"
-    variant="secondary"
-  >
-    <span className="text-primary mr-1">✦</span> My Projects
-  </Badge>
-  <h2 className="from-foreground to-foreground/80 max-w-[600px] bg-gradient-to-r bg-clip-text text-3xl font-bold tracking-tight text-transparent md:text-4xl">
-    A showcase of my work
-  </h2>
-  <p className="text-muted-foreground max-w-[500px] md:text-lg">
-    Here are some of the fullstack and desktop applications I’ve built — ranging from commercial management platforms to portfolio websites.
-  </p>
-</motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5 }}
+        className="mb-12 flex flex-col items-center justify-center space-y-4 text-center"
+      >
+        <Badge
+          className="rounded-full px-4 py-1.5 text-sm font-medium shadow-sm"
+          variant="secondary"
+        >
+          <span className="text-primary mr-1">✦</span> My Projects
+        </Badge>
+        <h2 className="from-foreground to-foreground/80 max-w-[600px] bg-gradient-to-r bg-clip-text text-3xl font-bold tracking-tight text-transparent md:text-4xl">
+          A showcase of my work
+        </h2>
+        <p className="text-muted-foreground max-w-[500px] md:text-lg">
+          Here are some of the fullstack and desktop applications I’ve built — ranging from commercial management platforms to portfolio websites.
+        </p>
+      </motion.div>
 
 
       <div className="container mx-auto grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-3 px-5 md:px-14 ">
-      {projects.map((project) => (
-        <FollowerPointerCard
-          key={project.slug}
-        >
-          <div className="group relative h-full overflow-hidden rounded-2xl border border-border bg-background transition duration-200 hover:shadow-xl">
-            <div className="relative aspect-[16/10] w-full overflow-hidden rounded-tl-lg rounded-tr-lg bg-muted">
-              <img
-                src={project.image}
-                alt="thumbnail"
-                className="h-full w-full object-cover transition duration-200 group-hover:scale-95 group-hover:rounded-2xl"
-              />
-            </div>
-            <div className="p-4 flex flex-col h-full">
-              <h2 className="my-2 text-lg font-bold text-foreground">
-                {project.title}
-              </h2>
-              <p className="mb-4 text-sm text-muted-foreground">
-                {project.description}
-              </p>
-
-              {/* Logos des technologies */}
-              <div className="mb-4 flex flex-wrap gap-3">
-                {project.technologies.map((tech, idx) => (
-                  <i
-                    key={idx}
-                    className={`${tech} text-2xl`}
-                  ></i>
-                ))}
+        {projects.map((project) => (
+          <FollowerPointerCard
+            key={project.slug}
+          >
+            <div className="group relative h-full overflow-hidden rounded-2xl border border-border bg-background transition duration-200 hover:shadow-xl">
+              <div className="relative aspect-[16/10] w-full overflow-hidden rounded-tl-lg rounded-tr-lg bg-muted">
+                <img
+                  src={project.image}
+                  alt="thumbnail"
+                  className="h-full w-full object-cover transition duration-200 group-hover:scale-95 group-hover:rounded-2xl"
+                />
               </div>
+              <div className="p-4 flex flex-col h-full">
+                <h2 className="my-2 text-lg font-bold text-foreground">
+                  {project.title}
+                </h2>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  {project.description}
+                </p>
 
-              {/* Boutons */}
-              <div className="mt-5 justify-around flex gap-3">
-                <a
-                  href={project.demo}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-2 rounded-lg bg-primary px-4 py-2 text-xs font-medium text-primary-foreground transition hover:bg-primary/90"
-                >
-                  <Globe className="h-4 w-4" />
-                  Browse Project
-                </a>
-                <a
-                  href={project.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center gap-2 rounded-lg border border-border px-4 py-2 text-xs font-medium text-foreground transition hover:bg-muted"
-                >
-                  <Github className="h-4 w-4" />
-                  View on GitHub
-                </a>
+                {/* Logos des technologies */}
+                <div className="mb-1 md:mb-2 flex flex-wrap gap-3">
+                  {project.technologies.map((tech, idx) => (
+                    <i
+                      key={idx}
+                      className={`${tech} text-2xl`}
+                    ></i>
+                  ))}
+                </div>
+
+                {/* Boutons */}
+                <div className="mt-5 md:justify-around flex gap-3 justify-end">
+                  <a
+                    href={project.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 rounded-lg bg-primary px-4 py-2 text-xs font-medium text-primary-foreground transition hover:bg-primary/90"
+                  >
+                    <Globe className="h-4 w-4" />
+                    <span className="hidden md:block">Browse Project</span>
+                  </a>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 rounded-lg border border-border px-4 py-2 text-xs font-medium text-foreground transition hover:bg-muted"
+                  >
+                    <Github className="h-4 w-4" />
+                    <span className="hidden md:block">View on GitHub</span>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </FollowerPointerCard>
-      ))}
-    </div>
+          </FollowerPointerCard>
+        ))}
+      </div>
     </div>
   );
 }
@@ -188,7 +188,7 @@ const projects = [
     title: "Event Booking Backend",
     description:
       "A RESTful API built with Express.js for managing event bookings. The backend handles user authentication, event creation, and ticket reservations. Ideal for powering event management platforms with scalable and maintainable code.",
-    image: "/projects/event-booking.png",
+    image: "/6.png",
     technologies: [
       "devicon-javascript-plain colored",
       "devicon-express-original",
