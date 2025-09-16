915eca333a8781f9dbb43d9b5a347ab0e2d720e8
MouadSadik <sadikmoad90@gmail.com>
2025-09-16 15:09:31 +0100

    Hola

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@
 
 import About from "@/components/home/about";
 import Education from "@/components/home/education";
-import Footer from "@/components/home/footer";
 import { Hero } from "@/components/home/hero";
-import { Header } from "@/components/home/navbar";
 import { Projects } from "@/components/home/projects";
 import { Skills } from "@/components/home/skills";
 
 import { useState } from "react";
 
 export default function Home() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  //const [isScrolled, setIsScrolled] = useState(false);
+  //const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div>
       
@@ -26,7 +24,6 @@ export default function Home() {
           <Education />
           <Skills />
           <Projects />
-          <Footer />
         </main>
     </div>
   );
